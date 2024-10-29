13e44384a9f44bb7d3839321a37807aeadd2daf6
DaniilSukhanov <82699248+DaniilSukhanov@users.noreply.github.com>
2024-10-29 19:20:20 +0300

    fix

---
+++ HW3_Reference/AppColor 2.swift
@@ -0,0 +1,20 @@
+//
+//  AppColor.swift
+//  HW3_Reference
+//
+//  Created by Даниил Суханов on 27.10.2024.
+//
+
+import UIKit
+
+enum AppColor {
+    static let firstBackgroundColor = UIColor(red: 0, green: 0, blue: 1, alpha: 1)
+    static let secondBackgroundColor = UIColor(red: 1, green: 0, blue: 1, alpha: 1)
+    static let black = UIColor.black
+    static let gray = UIColor.gray
+    static let white = UIColor.white
+    static let titleTextColor = UIColor.white
+    static let tableBackgroundColor = UIColor.blue
+    static let buttonBackgroundColor = UIColor(red: 0, green: 122.0/255.0, blue: 1, alpha: 1)
+    static let buttonTextColor = UIColor.white
+}
